5be5664b00a43b0af34116b2f0c861686794da85
Piyush Chatterjee <capsicum.broth@gmail.com>
2026-01-05 12:16:49 +0000

    inital_config

---
+++ src/logic.js
@@ -0,0 +1,23 @@
+const canvas=document.querySelector('canvas');
+// console.log(canvas);
+const context=canvas.getContext('2d');
+canvas.width=window.innerWidth;
+canvas.height=window.innerHeight;
+
+class Boundary{
+    constructor({pos}){
+        this.pos=pos;
+        this.width=40;
+        this.height=40;
+    }
+    draw(){
+        context.fillStyle='blue';
+        context.fillRect(this.pos.x,this.pos.y,this.width,this.height);
+    }
+}
+
+const b1=new Boundary({pos:{
+    x:0,
+    y:0
+}})
+b1.draw();
